f24738eed8b09b534c8471d9c70c62992abc3ad0
Prasadnair <prasadnair44@gmail.com>
2024-05-25 23:13:13 -0400

    exception handling in ,NET 8.0

---
+++ IExceptionHandlerTutorial/IExceptionHandlerTutorial/Program.cs
@@ -0,0 +1,38 @@
+using IExceptionHandlerTutorial.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Exception handling
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/ExceptionHandler/", () =>
+{
+    throw new Exception("Testing the IExceptionHandler in .NET 8.0");
+})
+.WithName("ThrowException")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
